f276a33b7f5bf683e92c9ea0a19f726f030e129e
Jonathan Llewellyn <inexistenz@gmail.com>
2022-11-29 08:04:56 -0500

    added list of source files in build.sh script

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,9 @@
 PROJECT_NAME=$1
 SOURCE_PATH=/src/code
 
+# List source paths
+ls -Rl $SOURCE_PATH
+
 # Build
 cmake -S . -B /build -DPROJECT_NAME=$PROJECT_NAME -DSOURCE_PATH=$SOURCE_PATH
 cmake --build /build
