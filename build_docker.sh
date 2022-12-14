2bc217a197303799219ffa7155fc8a2878713737
Jonathan Llewellyn <inexistenz@gmail.com>
2022-12-14 00:53:16 -0500

    build_docker.sh: using $PWD variable instead of '.'

--- build_docker.sh
+++ build_docker.sh
@@ -2,4 +2,4 @@
 # Build docker image named comp371:W22 from current directory
 # using Dockerfile.comp371
 
-docker build . -f Dockerfile.comp371 -t comp371:W22
+docker build "${PWD}" -f Dockerfile.comp371 -t comp371:W22
