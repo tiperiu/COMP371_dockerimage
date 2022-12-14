f86c516382c29c0ec67ceec4499e65aaf53a79be
Jonathan Llewellyn <inexistenz@gmail.com>
2022-12-14 00:53:36 -0500

    rebuild.sh: sourcing from script directory

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Delete the COMP371 container and image and rebuild
 
-./delete_container.sh
+"$(dirname ${BASH_SOURCE[0]})"/delete_container.sh
 
-docker rmi comp371:W22 # Delete image
+"$(dirname ${BASH_SOURCE[0]})"/delete_image.sh
 
-./build_docker.sh
+"$(dirname ${BASH_SOURCE[0]})"/build_docker.sh
