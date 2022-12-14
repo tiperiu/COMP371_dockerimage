1cf9bd64f90f471f5d91c7d4f6c35c6d37f7f19b
Jonathan Llewellyn <inexistenz@gmail.com>
2022-12-14 00:44:18 -0500

    renamed docker/build.sh to docker/build_and_run.sh

---
+++ docker/build_and_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Build and run the program using the example CMakeLists.txt. The program is
+# named after the first argument and the source code is located at /COMP371
+
+PROJECT_NAME="$1"
+SOURCE_PATH=/COMP371
+
+# List source paths
+ls -Rl $SOURCE_PATH
+
+# Build
+cmake -S /examples -B /build -DPROJECT_NAME="${PROJECT_NAME}" -DSOURCE_PATH="${SOURCE_PATH}"
+cmake --build /build
+
+# Run
+/build/"${PROJECT_NAME}"
