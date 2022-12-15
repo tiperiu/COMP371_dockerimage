e266698c658694f0eb77a944f173e7caf438ad5e
Jonathan Llewellyn <inexistenz@gmail.com>
2022-12-14 23:17:30 -0500

    build_and_run: now lists contents of $SOURCE_PATH

--- docker/build_and_run.sh
+++ docker/build_and_run.sh
@@ -6,7 +6,8 @@ PROJECT_NAME="$1"
 SOURCE_PATH=/COMP371
 
 # List source paths
-ls -Rl $SOURCE_PATH
+echo "Listing "${SOURCE_PATH} Contents..."
+ls -Rl ${SOURCE_PATH}/*
 
 # Build
 cmake -S /examples -B /build -DPROJECT_NAME="${PROJECT_NAME}" -DSOURCE_PATH="${SOURCE_PATH}"
